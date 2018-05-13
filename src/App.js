4bfb5f5387bb8cf152f0cf085d53a0daf5213b19
Alicia Key <alicia@akey7.com>
2018-05-12 18:41:48 -0600

    FINISHED YAY!!!!!

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import React from 'react'
 import './App.css'
-import { actions } from './store-and-actions'
-import { connect } from 'unistore/react'
 import NumberDisplay from './number-display-component'
 import UpDown from './updown-component'
 
-const App = ({ count, increment, decrement }) => (
-  <div className="App">
+const App = () => (
+  <div className='App'>
     <NumberDisplay />
     <UpDown />
   </div>
 )
 
-export default connect('count', actions)(App)
+export default App
